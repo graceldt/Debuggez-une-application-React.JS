f9da7914d4edecf99bfc4c1312d527f120060b55
graceldt <grace.lendet@gmail.com>
2024-08-11 13:57:45 +0200

    modification pagination

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -63,7 +63,7 @@ const EventList = () => {
           <div className="Pagination">
             {[...Array(pageNumber || 0)].map((_, n) => (
               // eslint-disable-next-line react/no-array-index-key
-              <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
+              <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)} className={currentPage === n+1 ? "active" : "none"}>
                 {n + 1}
               </a>
             ))}
